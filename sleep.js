5ad1f9ce32cef9e6d5bf8d094619851edd7c5eab
airportyh <airportyh@gmail.com>
2013-06-03 17:29:56 -0400

    Added sleep example.

---
+++ sleep.js
@@ -0,0 +1,30 @@
+// Synchronous-looking sleep
+
+// `run` is a top level
+function run(genfun){
+  var gen = genfun()
+  function next(){
+    var res = gen.next()
+    if (!res.done){
+      res.value(next)
+    }
+  }
+  next()
+}
+
+
+// A "continueable-ish" type of thing that
+// when yielded to will sleep for `ms` milliseconds.
+function sleep(ms){
+  return function(callback){
+    setTimeout(callback, ms)
+  }
+}
+
+run(function *(){
+  for (var i = 10; i > 0; i--){
+    console.log(i)
+    yield sleep(1000)
+  }
+  console.log('Happy New Year!')
+})
